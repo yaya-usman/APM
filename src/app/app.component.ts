ef793ba8b1ceb3f014d997d203df90e82ce7a53c
Elixir-K <yayaazeez222@gmail.com>
2021-10-07 23:29:24 +0300

    product-list custom-directives in template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   template: `
   <div>
     <h1>{{ pageTitle }}</h1>
-    <div>My first Component</div>
-  </div> `,
+    <pm-products></pm-products>
+    </div> `,
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
