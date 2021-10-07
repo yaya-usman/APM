d67ea8b42f52fb48fd0531424d7e1c62c67d52a6
Elixir-K <yayaazeez222@gmail.com>
2021-10-07 22:41:53 +0300

    product-list template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'pm-root',
   template: `
-  <div style="text-align:center">
+  <div>
     <h1>{{ pageTitle }}</h1>
     <div>My first Component</div>
   </div> `,
