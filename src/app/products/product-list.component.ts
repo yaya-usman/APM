458c0a8a614267d82ba3e12dd89f49e48622e9b1
Elixir-K <yayaazeez222@gmail.com>
2021-10-10 00:13:29 +0300

    Product interface and encapsulated css for product component

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import {IProduct} from './product'
 
 @Component({
   selector: 'pm-products',
   templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
   imageWidth: number = 50;
   imageMargin: number = 2;
   showImage: boolean = false;
-  products: any[] = [
+  listFilter: string = '';
+  products: IProduct[] = [
     {
       productId: 1,
       productName: 'Leaf Rake',
@@ -44,5 +47,9 @@ export class ProductListComponent {
 
   toggleImage():void{
     this.showImage = !this.showImage;
+  }
+
+  ngOnInit():void{
+    console.log("Hello it's OnInit");
   }
 }
