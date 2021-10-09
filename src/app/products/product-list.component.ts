127dc277e324685c4c9805c7d796ef90c221fa98
Elixir-K <yayaazeez222@gmail.com>
2021-10-09 23:05:43 +0300

    image property binding and handle click event

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -6,6 +6,9 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
   pageTitle: string = 'Product List';
+  imageWidth: number = 50;
+  imageMargin: number = 2;
+  showImage: boolean = false;
   products: any[] = [
     {
       productId: 1,
@@ -38,4 +41,8 @@ export class ProductListComponent {
       imageUrl: 'assets/images/hammer.png',
     },
   ];
+
+  toggleImage():void{
+    this.showImage = !this.showImage;
+  }
 }
