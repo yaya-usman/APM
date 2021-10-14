cca0584ab3c12f32ab0aede6b5c75c0bf500a85a
Elixir-K <yayaazeez222@gmail.com>
2021-10-15 00:51:28 +0300

    route update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'pm-root',
-  template: `
-  <div>
-    <h1>{{ pageTitle }}</h1>
-    <pm-products></pm-products>
-    </div> `,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   pageTitle: string = 'Acme Product Management';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,8 +24,9 @@ import { ProductDetailComponent } from './products/product-detail.component';
     HttpClientModule,
     RouterModule.forRoot([
       { path: 'products', component: ProductListComponent },
-      { path: '/products/:id', component: ProductDetailComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: 'products/:id', component: ProductDetailComponent },
+      { path: 'welcome', component: WelcomeComponent},
+      { path: '', redirectTo: 'welcome',pathMatch: 'full' },
       { path: '**', redirectTo: 'welcome', pathMatch: 'full' },
     ]),
   ],
